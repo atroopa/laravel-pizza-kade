9f400da8b7a497832931f28fcfdddbdb0ee2a0a5
atroopa <atroopa@gmail.com>
2024-06-13 19:50:42 +0330

    add image to welcome

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -17,5 +17,8 @@
 
         @yield('content')
 
+        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </footer>
     </body>
 </html>
