dab6c4714ffc7d675d44b22962c4a132a5ccad9e
atroopa <atroopa@gmail.com>
2024-06-13 19:25:47 +0330

    add layout to project

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-        <title>Pizzas</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body>
+@section('content')
+           
        <div class="flex flex-col items-center justify-center">
         <h3 class="text-2xl mt-10 font-bold text-red-900">Pizza List</h3>
 
@@ -40,5 +28,4 @@
 
     </div>
 
-    </body>
-</html>
+@endsection
